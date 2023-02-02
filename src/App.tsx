19975929613892977995a0df6b8c8d58d9f6b870
soapkelele <buev_anton@mail.ru>
2023-02-02 21:21:52 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import MainBlock from 'components/MainBlock'
-import Root from 'components/Root'
 
 export default function () {
-  return (
-    <Root>
-      <MainBlock />
-    </Root>
-  )
+  return <MainBlock />
 }
