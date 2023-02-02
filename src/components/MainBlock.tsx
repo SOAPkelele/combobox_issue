be0cd46cc0774374e3b70025335be145fb806639
soapkelele <buev_anton@mail.ru>
2023-02-02 20:30:30 +0300

    upd

--- src/components/MainBlock.tsx
+++ src/components/MainBlock.tsx
@@ -1,28 +1,71 @@
-import { BodyText, HeaderText } from 'components/Text'
-import { Suspense } from 'preact/compat'
-import UserCount from 'components/UserCount'
-import classnames, {
-  alignItems,
-  display,
-  flexDirection,
-  justifyContent,
-  space,
-} from 'classnames/tailwind'
+import { Combobox } from '@headlessui/react'
+import { useState } from 'react'
 
-const container = classnames(
-  display('flex'),
-  flexDirection('flex-col'),
-  justifyContent('justify-center'),
-  alignItems('items-center'),
-  space('space-y-2')
-)
-export default function () {
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+]
+
+function ComboboxField({ name }: { name: string }) {
+  const [selected, setSelected] = useState(people[0])
+  const [query, setQuery] = useState('')
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) =>
+          person.name
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        )
+
   return (
-    <div className={container}>
-      <HeaderText>Frontend Template</HeaderText>
-      <Suspense fallback={<BodyText>Loading...</BodyText>}>
-        <UserCount />
-      </Suspense>
-    </div>
+    <Combobox value={selected} onChange={setSelected} name={name}>
+      <div className="relative mt-1">
+        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <Combobox.Input
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            displayValue={(person) => person.name}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </div>
+
+        <Combobox.Options className="ring-black absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-opacity-5 focus:outline-none sm:text-sm">
+          {filteredPeople.length === 0 && query !== '' ? (
+            <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+              Nothing found.
+            </div>
+          ) : (
+            filteredPeople.map((person) => (
+              <Combobox.Option
+                key={person.id}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                  }`
+                }
+                value={person}
+              >
+                {person.name}
+              </Combobox.Option>
+            ))
+          )}
+        </Combobox.Options>
+      </div>
+    </Combobox>
+  )
+}
+
+export default function Form() {
+  return (
+    <form action="" className="my-auto flex h-screen flex-col">
+      <ComboboxField name="field_one" />
+      <ComboboxField name="field_two" />
+    </form>
   )
 }
